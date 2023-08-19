c2e190b939c3731bd7d87653e05418437b4cb8a7
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-19 15:23:10 +0300

    print_ch_str.c is function containing the function that prints char,string, %

---
+++ print_ch_str.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * spf_char - print char
+ * @args: List a of arguments
+ * Return: zero
+ */
+int spf_char(va_list args)
+{
+	char c_spf;
+
+	c_spf = va_arg(args, int);
+
+	_putchar(c_spf);
+
+	return (0);
+}
+
+/**
+ * spf_str - prints string
+ * @args: list of arguments
+ * Return: zero
+ */
+int spf_str(va_list args)
+{
+	int len = 0, i;
+	char *str;
+
+	str = va_arg(args, char *);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(*str);
+		len++;
+	}
+
+	return(len);
+}
+
