add1ebeae349c6eb8cd3bac19692c66a865ff422
Nahom kiflu <kiflunahom1994@gmail.com>
2023-08-21 10:14:37 +0300

    adding folder to test different scinareo

---
+++ for_testing/prints_fsp.c
@@ -0,0 +1,8 @@
+/**
+ * prints - function that prints arguments
+ * @format: the string
+ * @arg: the arguments
+ * Description - this function links operators to actions
+ * Return: string length
+ */
+
